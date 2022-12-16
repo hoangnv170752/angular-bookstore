041eddc7adf3e79634920b41ff0471dfccff66a0
hoangnv170752 <hoang.nv.ral@gmail.com>
2022-12-16 17:53:30 +0700

    btvn angular 1

---
+++ README.md
@@ -0,0 +1,6 @@
+## Tính năng web app ##
+1. Sidebar menu
+- Routing qua Quản lý sách 
+- Routing qua Thêm mới sách
+2. Toolbar menu
+3. Footer
---
+++ bookstoreUI/src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './navbar/navbar.component';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import {MatListModule} from '@angular/material/list';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    AboutComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSlideToggleModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatListModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
